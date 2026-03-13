fb96286f66ffb01a41c84149d463fe8033305575
Mustafa Mathhar <laraahmmad37@gmail.com>
2026-03-13 14:43:11 +0300

    update for render

--- app.py
+++ app.py
@@ -8,24 +8,32 @@ app = Flask(__name__)
 
 UPLOAD_FOLDER = "uploads"
 COMPRESSED_FOLDER = "compressed"
+
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 os.makedirs(COMPRESSED_FOLDER, exist_ok=True)
 
-GS_PATH = r"C:\Program Files\gs\gs10.06.0\bin\gswin64c.exe" 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+
     if request.method == "POST":
+
         file = request.files["file"]
+
+        if file.filename == "":
+            return "No file selected"
+
         input_path = os.path.join(UPLOAD_FOLDER, file.filename)
         file.save(input_path)
 
         filename, ext = os.path.splitext(file.filename)
         ext = ext.lower()
 
-        # PDF compression using Ghostscript
+        # PDF compression
         if ext == ".pdf":
+
             output_path = os.path.join(COMPRESSED_FOLDER, f"compressed_{file.filename}")
+
             try:
                 subprocess.run([
                     "gs",
@@ -43,23 +51,43 @@ def index():
 
         # Image compression
         elif ext in [".jpg", ".jpeg", ".png"]:
+
             output_path = os.path.join(COMPRESSED_FOLDER, f"compressed_{file.filename}")
+
             img = Image.open(input_path)
+
             if ext in [".jpg", ".jpeg"]:
                 img.save(output_path, "JPEG", quality=70, optimize=True)
             else:
                 img.save(output_path, "PNG", optimize=True)
 
-        else:  # Other files
-            output_path = os.path.join(COMPRESSED_FOLDER, f"compressed_{file.filename}.zip")
-            with zipfile.ZipFile(output_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        # Other files
+        else:
+
+            output_path = os.path.join(COMPRESSED_FOLDER, f"{filename}.zip")
+
+            with zipfile.ZipFile(output_path, "w", zipfile.ZIP_DEFLATED) as zipf:
                 zipf.write(input_path, arcname=file.filename)
 
-        return send_file(output_path, as_attachment=True)
+        response = send_file(output_path, as_attachment=True)
+
+        # حذف الملفات بعد الإرسال
+        try:
+            os.remove(input_path)
+            os.remove(output_path)
+        except:
+            pass
+
+        return response
 
     return render_template("index.html")
 
+
 if __name__ == "__main__":
-   
+
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
